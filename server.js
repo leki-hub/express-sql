161a0a66c1b88848397a2c6d50ec40be4b4a383b
Leki <lekisintole82@gmail.com>
2024-04-08 22:41:04 +0300

    server pot reconfiguration

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ const server = require('http')
 const app = require('./app')
 const port = 3300
 // Start the server on the specified port
-server.createServer(app).listen(port)
+server.createServer(app)
+server.listen(port)
