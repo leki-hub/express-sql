66aee87884e1125f85e278d872c9c373a05954f4
Leki <lekisintole82@gmail.com>
2024-04-08 21:55:03 +0300

    body-parser use

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bodyParser= require( 'body-parser' )
+
 const app = express()
   
 const postsRoute = require( './routes/posts' )  
+app.use(bodyParser.json)
 app.use('/posts', postsRoute)
 
 module.exports= app
