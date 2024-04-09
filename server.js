4eab0f2ea760c680110672af7c57614f9e58d556
Leki <lekisintole82@gmail.com>
2024-04-10 00:14:50 +0300

    server pot

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const server = require('http')
 const app = require('./app')
-const port = 3300
+const port = 3000
 // Start the server on the specified port
-server.createServer(app)
-server.listen(port)
+server.createServer(app).listen(port)
