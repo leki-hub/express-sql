b8ad50611e9adf2bf193d0cbe04fe832c008bc8b
Leki <lekisintole82@gmail.com>
2024-04-09 23:53:41 +0300

    post route, callback fn logic

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,8 +1,32 @@
-function index(req, res) {
-    const post= 'Data to be posted'
-    res.send(post)
+const models = require('../models');
+
+function save(req, res) {
+    const post= {
+        title: req.body.title,
+        content: req.body.content,
+        imageUrl:req.body.imageUrl,
+        categoryId:req.body.categoryId,
+        userID: req.body.userID
+
+    }
+    models.Post.create(post).then(result  =>{
+        res.status(201).json({
+            message:'Post created successully',
+            post:result
+        
+        })
+    }
+
+    )
+        .catch((err)=>{
+            res.status(500).json({
+                message:'Something went wrong',
+                error:err})
+        })
+       
+
 }
 
 module.exports= {
-    index:index
+    savePost:save
 }
