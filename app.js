e431d62cd50e4c2836126cdbfd90c003c9546515
Leki <lekisintole82@gmail.com>
2024-04-10 00:12:13 +0300

    express app

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ const app = express()
 const postsRoute = require( './routes/posts' )  
 app.use(bodyParser.json)
 app.use('/posts', postsRoute)
+// app.get('/', (req, res)=>{
+//     res.send({message: "Welcome"})
+// })
+app.get('/blog', (req, res)=>{
+    res.send('Blog Page')
+})
+
 
 module.exports= app
