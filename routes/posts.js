95de3e01a3e639fb722313a421f7135fd50b6097
Leki <lekisintole82@gmail.com>
2024-04-11 11:05:27 +0300

    get single post

--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,6 @@ const postController = require('../controllers/post.controller');
 const router = express.Router()
 
  router.post('/', postController.savePost)
+ router.get('/:id', postController.show)
 
  module.exports = router;
