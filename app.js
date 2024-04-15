2234dd087a37c9fbb2047c928c157f04f2a4bd65
Leki <lekisintole82@gmail.com>
2024-04-15 15:01:37 +0300

    body-parser correct set up

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser= require( 'body-parser' )
 const app = express()
   
 const postsRoute = require( './routes/posts' )  
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 app.use('/posts', postsRoute)
-// app.get('/', (req, res)=>{
-//     res.send({message: "Welcome"})
-// })
+app.get('/', (req, res)=>{
+    res.send({message: "Welcome"})
+})
 app.get('/blog', (req, res)=>{
     res.send('Blog Page')
 })
