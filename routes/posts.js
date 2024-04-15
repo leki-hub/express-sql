a01598e042a852596e54d72152b354e7d58adaa5
Leki <lekisintole82@gmail.com>
2024-04-15 15:23:53 +0300

    patch and delete

--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,7 @@ const router = express.Router()
  router.post('/', postController.savePost)
  router.get('/:id', postController.show)
  router.get('/', postController.index)
+ router.patch('/:id', postController.updatePost)
+ router.delete('/:id', postController.delete)
 
  module.exports = router;
