b9cb152b602b7f543200d998045a35da75962431
Leki <lekisintole82@gmail.com>
2024-04-15 22:15:47 +0300

    sign up logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,24 @@
 const models = require( '../models' );
 const bcryptjs= require( "bcrypt" ) ;
+const jwt = require("jsonwebtoken");
 
+function   signup ( req, res ){
+  
+    const  user = {
+        name: req.body.name,
+        email:req.body.email ,
+        password: req.body.password
+    }
+  
+
+    models.User.create(user).then(result=>{
+        res.status(201).send({message:"User Created successflly", data : result});
+        
+    }).catch((err)=>{
+        res.status(500).send({ message: "Something went wrong",
+        error: err})
+   
+    })
+    })
+
+}
