ba4071106716a0bbe5c0b634a0e1fd65793153a5
Leki <lekisintole82@gmail.com>
2024-04-16 11:57:28 +0300

    no sign up using same email

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,29 +1,57 @@
 const models = require( '../models' );
-const bcryptjs= require( "bcrypt" ) ;
 const jwt = require("jsonwebtoken");
+const bcryptjs= require( "bcrypt" ) ;
 
-function   signup ( req, res ){
-  
-    const  user = {
-        name: req.body.name,
-        email:req.body.email ,
-        password: req.body.password
-    }
-  
 
-    models.User.create(user).then(result=>{
-        res.status(201).send({message:"User Created successflly", data : result});
+function signup( req, res ){
+
+
+    models.User.findOne({ where:{ email : req.body.email }}).then(user => {
+        if (user) {
+            res.status(409).send({message:'Email is already in use'})
+        }
+        else{
+            bcryptjs.genSalt(10, function ( err, salt ) {
+                bcryptjs.hash(req.body.password , salt, function (err,hash){
+                
+                    
+            const  user = {
+                name: req.body.name,
+                email:req.body.email ,
+                password: hash
+            }
         
-    }).catch((err)=>{
+            models.User.create(user).then(result=>{
+                res.status(201).send({message:"User Created successflly"});
+                
+            }).catch((err)=>{
+                res.status(500).send({ message: "Something went wrong",
+                error: err})
+           
+            })
+            
+                }) 
+            })
+        
+        }
+  
+    
+    })
+    .catch((err)=>{
         res.status(500).send({ message: "Something went wrong",
         error: err})
    
     })
-    
+
+
+
+
 
 }
 
 
-models.exports  = {
+
+
+module.exports  = {
     signup:signup,
 }
