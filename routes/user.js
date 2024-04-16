49bafd04c61ce04037a1ee0193a4eaba3be652dc
Leki <lekisintole82@gmail.com>
2024-04-16 09:39:42 +0300

    user sign up route

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,6 @@
 const express = require( 'express' );
+const userController =  require('../controllers/user.controller')
 const app = express()
 const router = express.Router();
+
+router.post('/sign-up', userController.signup) //
