d2d6315ad1f6853b0803a5bf6c825d7c43477489
Leki <lekisintole82@gmail.com>
2024-04-16 09:55:30 +0300

    user route middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser= require( 'body-parser' )
-
+const postsRoute = require( './routes/posts' ) 
+const userRoute = require( './routes/user' )
 const app = express()
   
-const postsRoute = require( './routes/posts' )  
+
 app.use(bodyParser.json())
 app.use('/posts', postsRoute)
+app.use('/users', userRoute)
 app.get('/', (req, res)=>{
     res.send({message: "Welcome"})
 })
