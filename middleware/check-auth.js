bf0e411fcb968cfef8a7e0dd6cdb63a984de0ddb
Leki <lekisintole82@gmail.com>
2024-04-19 23:55:32 +0300

    protect routes

---
+++ middleware/check-auth.js
@@ -0,0 +1,11 @@
+const jwt = require( "jsonwebtoken" );
+
+function  verifyToken( req, res, next ) {
+    try {
+        let token = req.headers.authorization;
+        
+    } catch (error) {
+        
+    }
+    
+}
