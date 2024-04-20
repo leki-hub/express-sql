c0c5241c65d6daf18c596f8d9b6d5b792a218d81
Leki <lekisintole82@gmail.com>
2024-04-21 00:33:39 +0300

    secret key

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,10 +2,22 @@ const jwt = require( "jsonwebtoken" );
 
 function  verifyToken( req, res, next ) {
     try {
-        let token = req.headers.authorization;
+        let token = req.headers.authorization.split( ' ')[1]; // 'Bearer' <token as in re.header
+        const decodedToken= jwt.verify( token, "setsecretkeyfrom_env");
+        req.userData = decodedToken;
+        next();
         
-    } catch (error) {
+    } catch (err) {
+        return res.status(401).json(
+            { message: 'Invalid or expired token provided' ,
+            error : err
+           }
+        );
         
     }
     
+}
+
+module.exports = {
+    checkAuth:verifyToken
 }
