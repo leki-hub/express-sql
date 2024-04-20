f60698f7f9bcfcae7c87d6b41da83af6d8440321
Leki <lekisintole82@gmail.com>
2024-04-21 00:34:56 +0300

    protect routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 const express = require( 'express' );
 const postController = require('../controllers/post.controller');
+const checkAuthMiddleware  = require("../middleware/check-auth");
 const router = express.Router()
 
- router.post('/', postController.savePost)
+ router.post('/',checkAuthMiddleware.checkAuth, postController.savePost)
  router.get('/:id', postController.show)
  router.get('/', postController.index)
- router.patch('/:id', postController.updatePost)
- router.delete('/:id', postController.delete)
+ router.patch('/:id',checkAuthMiddleware.checkAuth, postController.updatePost)
+ router.delete('/:id',checkAuthMiddleware.checkAuth, postController.delete)
 
  module.exports = router;
